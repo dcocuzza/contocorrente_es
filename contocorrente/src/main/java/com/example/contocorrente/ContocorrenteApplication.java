b62b31149a718a8ba118b7b555e781d623587ec3
daniele cocuzza <daniele.cocuzza@aitho.it>
2022-12-27 09:06:38 +0100

    aggiunti file progetto

---
+++ contocorrente/src/main/java/com/example/contocorrente/ContocorrenteApplication.java
@@ -0,0 +1,13 @@
+package com.example.contocorrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContocorrenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContocorrenteApplication.class, args);
+	}
+
+}
